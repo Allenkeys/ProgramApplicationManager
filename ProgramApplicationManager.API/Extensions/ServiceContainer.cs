863a593f6a7a83b84528b0efaa390f98c1ee7fb8
Joshua Eze <joshua.eze@cydene.com>
2024-05-19 11:35:30 +0100

    Added database connections

---
+++ ProgramApplicationManager.API/Extensions/ServiceContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramApplicationManager.Persistence;
+
+namespace ProgramApplicationManager.API.Extensions
+{
+    public static class ServiceContainer
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseCosmos(
+                    config.GetSection("DbConnection")["dburl"],
+                    config.GetSection("DbConnection")["key"],
+                    config.GetSection("DbConnection")["dbname"]
+                );
+            });
+        }
+    }
+}
