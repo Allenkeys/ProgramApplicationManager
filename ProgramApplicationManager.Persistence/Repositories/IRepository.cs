76db2d072184659ff8f9390c5a0d0d07ad533274
Joshua Eze <joshua.eze@cydene.com>
2024-05-19 12:26:40 +0100

    Added repository

---
+++ ProgramApplicationManager.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramApplicationManager.Persistence.Repositories
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll(bool trackChanges);
+        T Create(T entity);
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate, bool trackChanges);
+    }
+}
